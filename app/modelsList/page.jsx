4beb48ecb349e99e8ad447e3284a32002aa80f43
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-05-08 09:49:18 +0400

    a

--- app/modelsList/page.jsx
+++ app/modelsList/page.jsx
@@ -2,12 +2,16 @@ import { Search } from 'lucide-react';
 
 export default function ModelsList() {
     return (
-        <div className='mt-25 m-auto w-[85%] max-w-[1200px]'>
-                <div className='flex flex-col'>
-                    <h1 className='text-6xl font-semibold '>
+        <div className="mt-15 mx-auto w-[85%] shadow rounded max-w-[1200px] h-90">
+            <div className="flex justify-between items-center h-full px-4">
+                {/* Text and Search Section */}
+                <div className="flex flex-col p-4 ">
+                    <h1 className="text-6xl font-semibold">
                         Available AI Models
                     </h1>
-                    <p className='text-gray-600 text-[18px] mt-5 mb-7'>Browse and download top pre-trained models</p>
+                    <p className="text-gray-600 text-lg mt-5 mb-7">
+                        Browse and download top pre-trained models
+                    </p>
                     <div className="flex items-center bg-gray-100 rounded-xl px-4 py-3 w-full max-w-md">
                         <Search className="w-6 h-6 text-gray-600 mr-2" />
                         <input
@@ -17,7 +21,16 @@ export default function ModelsList() {
                         />
                     </div>
                 </div>
-                <img src='herolist.png' alt='herolistPng' className='w-[23%] absolute right-[15%] mb-5 top-[10%]' />
+                {/* Image Section */}
+                <div className="w-1/3 flex justify-center h-full ">
+                    <img
+                        src="herolist.png"
+                        alt="herolist"
+                        className="w-full min-w-[350px] h-ful object-cover"
+                    />
+                </div>
+            </div>
         </div>
+
     )
 }
