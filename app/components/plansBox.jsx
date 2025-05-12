7257fdfd2f6e0c6afb74808f3785334590f57f5c
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-05-12 13:04:23 +0400

    respnisve left for the plans

--- app/components/plansBox.jsx
+++ app/components/plansBox.jsx
@@ -1,14 +1,26 @@
+import plansData from 'app/plansData'
+
 export default function plansBox() {
     return (
-        <div className='w-1/3 flex justify-center flex-col items-center border-gray-300 rounded-2xl border-1'>
-            <img className='w-1/2 ' src='plansImg1.png' alt='plans image 1'/>
-            <h1 className='font-bold text-2xl'>Free <br/> $<span>0<span className='font-light text-lg'>/mo</span></span> </h1>
-            <ul className='text-gray-600 flex flex-col gap-1 mt-3'>
-                <li>up to 5 uploads</li>
-                <li>up to 5 uploads</li>
-                <li>up to 5 uploads</li>
-            </ul>
-            <button className='text-lg btn-primary text-white px-10 py-3 rounded-xl mt-3 mb-5'>Get started</button>
+        <div className="flex gap-6"> {/* This wrapper is required */}
+            {plansData.map(plan => (
+                <div key={plan.id} className='w-1/3 flex justify-center flex-col items-center border-gray-300 rounded-2xl border-1'>
+                    <img className='w-1/2' src={plan.img.src} alt={plan.img.alt} />
+                    <h1 className='font-bold text-2xl'>
+                        {plan.status} <br />
+                        ${plan.price}
+                        <span className='font-light text-lg'>/mo</span>
+                    </h1>
+                    <ul className='text-gray-600 flex flex-col gap-1 mt-3 h-[90px]'>
+                        {plan.features.map((feature, index) => (
+                            <li key={index}>{feature}</li>
+                        ))}
+                    </ul>
+                    <button className='text-lg btn-primary text-white w-[190px] py-3 rounded-xl mt-3 mb-5'>
+                        {plan.buttonText}
+                    </button>
+                </div>
+            ))}
         </div>
     )
 }
