67b0cdf4c5869ee4154d09619179da60107ef805
Giorgi Dalakishvili <giorgidalakishvili@Giorgis-MacBook-Air.local>
2025-05-15 12:52:10 +0400

    ds12

--- app/components/modelBox.jsx
+++ app/components/modelBox.jsx
@@ -1,21 +1,23 @@
 import modelData from 'app/modelsList/modeldata.js'
+import { FiDownload } from 'react-icons/fi';
+import { AiOutlineHeart } from 'react-icons/ai';
 
 export default function modelBox() {
     return (
         <div className='flex gap-7 w-full'>
-            <div className='h-[700px] w-[400px] hidden md:block rounded-lg shadow'></div>
+            <div className='h-[700px] w-[400px] hidden lg:block rounded-lg cutom-shadow'></div>
             <section className='flex flex-col gap-7 w-full'>
                 {modelData.map(model => (
-                    <div key={model.id} className='shadow p-1 rounded-2xl overflow-hidden cursor-pointer hover:bg-gray-50 transition-all'>
+                    <div key={model.id} className='cutom-shadow p-1 rounded-2xl overflow-hidden cursor-pointer hover:bg-gray-50 transition-all max-w-[900px]'>
                         <div className='p-3.5 flex flex-col gap-5'>
                             <div className='flex gap-4'>
                                 {model.image && (
                                     <img src={model.image.src} 
                                     alt={model.image.alt} 
-                                    className='w-16 h-16 rounded-lg object-cover' />
+                                    className='w-12 h-12 sm:w-16 sm:h-16 rounded-lg object-cover' />
                                 )}
                                 <div className='flex flex-col gap-2'>
-                                    <h2 className='text-2xl font-semibold'>{model.name}</h2>
+                                    <h2 className='text-1xl sm:text-2xl font-semibold'>{model.name}</h2>
                                     <span className='flex gap-1 text-gray-500'>
                                      author: <p>{model.author}</p></span>
                                 </div>
@@ -26,6 +28,12 @@ export default function modelBox() {
                                         {tag}
                                     </div>
                                 ))}
+                            </div>
+                            {/* Revives */}
+                            <div className='flex items-center gap-4 font-light text-sm'>
+                                <p>Uploaded: <span>12.05.2</span></p>
+                                <p className='flex items-center gap-1'><FiDownload/><span>55</span></p>
+                                <p className='flex items-center gap-1'><AiOutlineHeart/><span>55</span></p>
                             </div>
                             <span className='flex gap-1'>
                                 Price: <span className='flex'>$<p>{model.price}</p></span>
--- app/modelsList/page.jsx
+++ app/modelsList/page.jsx
@@ -3,22 +3,21 @@ import ModelBox from 'app/components/modelBox';
 
 export default function ModelsList() {
     return (
-        <div className="mt-15 mx-auto w-[80%] max-w-[1500px] ">
-            <div className="flex justify-between items-center h-full px-4">
-                <div className="flex flex-col p-4 w-full">
-                    <h1 className="text-6xl font-semibold">
-                        Available AI Models
+        <div className="mt-15 mx-auto w-[80%] max-w-[1500px]">
+            <div className="flex h-full px-4">
+                <div className="flex flex-col items-center p-4 w-full">
+                    <h1 className="text-2xl mb-4 sm:mb-0 sm:text-5xl md:text-6xl font-semibold">
+                        Available <span style={{color:'#6472ef'}}>AI</span> Models
                     </h1>
-                    <p className="text-gray-600 text-lg mt-5 mb-7">
+                    <p className="text-gray-600 hidden sm:text-lg sm:block mt-4 mb-4">
                         Browse and download top pre-trained models
                     </p>
-                    <div className="flex items-center bg-gray-100 rounded-xl px-3 py-3 w-[75%] max-w-md">
+                    <div className="flex items-center bg-gray-100 rounded-xl px-3 p-1.5 sm:p-3 w-[100%] max-w-md">
                         <Search className="w-6 h-6 text-gray-600 mr-2" />
                         <input
                             type="text"
                             placeholder="Search for a model..."
-                            className="bg-transparent focus:outline-none w-full text-lg text-gray-800 placeholder-gray-600"
-                        />
+                            className="bg-transparent focus:outline-none w-full text-lg text-gray-800 placeholder-gray-600"/>
                     </div>
                 </div>
             </div>
