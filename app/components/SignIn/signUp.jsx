a98d408f30b865678ceda57aeb52904d315e85db
Dalaqa25 <g.dalaqishvili01@gmail.com>
2025-05-16 22:30:00 +0400

    mobile sign up need to be added

--- app/components/SignIn/signUp.jsx
+++ app/components/SignIn/signUp.jsx
@@ -8,27 +8,27 @@ export default function SignUp() {
     }
 
     return (
-        <div className="fixed top-1/2 left-1/2 z-50 bg-white rounded-2xl shadow -translate-x-1/2 -translate-y-1/2 min-w-[350px] min-h-[400px] flex flex-col items-center">
+        <div className="fixed top-1/2 left-1/2 z-50 bg-white rounded-2xl shadow -translate-x-1/2 -translate-y-1/2 flex flex-col items-center w-1/2 max-w-[400px] min-w-[320px] sm:text-lg text-sm">
             {/* Floating image */}
-            <div className="absolute -top-23 left-1/2 -translate-x-1/2">
-                <Image src='/3dcube.png' alt="logo.png" width={1024} height={1024} className="w-45 h-45" />
+            <div className="absolute -top-13 left-1/2 -translate-x-1/2 sm:-top-20">
+                <Image src='/3dcube.png' alt="logo.png" width={1024} height={1024} className="w-35 h-35 sm:w-40 sm:h-40" />
             </div>
             <section className="px-5 flex flex-col items-center mt-17">
-                <h1 className="text-4xl font-semibold">Sign Up</h1>
-                <p className="text-gray-600 font-light text-[18px]">Already have an account? <span className="underline cursor-pointer" onClick={() => setIsClicked(true)}>Log In!</span></p>
+                <h1 className="text-2xl sm:text-4xl font-semibold">Sign Up</h1>
+                <p className="text-gray-600 font-light">Already have an account? <span className="underline cursor-pointer" onClick={() => setIsClicked(true)}>Log In!</span></p>
             </section>
             <form action="">
-                <div className="flex flex-col gap-5 px-5 py-10">
+                <div className="flex flex-col gap-5 px-5 mb-10 mt-5">
                     <label htmlFor="Email" className="flex flex-col gap-0.5">Username or Email address
-                        <input id="Email" type="text" placeholder="Email" className="text-lg w-90 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                        <input id="Email" type="text" placeholder="Email" className="text-lg w-[95%] border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
                     </label>
                     <label htmlFor="Password" className="flex flex-col gap-0.5">Password
-                        <input type="password" placeholder="Password" className="text-lg border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                        <input type="password" placeholder="Password" className="text-lg border w-[95%] border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
                     </label>
                     <label htmlFor="Password" className="flex flex-col gap-0.5">Repeat Password
-                        <input type="password" placeholder="Password" className="text-lg border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                        <input type="password" placeholder="Password" className="text-lg border w-[95%] border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
                     </label>
-                    <button type="submit" className="text-lg bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200">Sign Up</button>
+                    <button type="submit" className="text-lg bg-blue-500 w-[95%] text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200">Sign Up</button>
                 </div>
             </form>
         </div>
