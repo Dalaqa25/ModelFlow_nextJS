359404b47f56a80e13b8a61397f656e583801f8d
Dalaqa25 <g.dalaqishvili01@gmail.com>
2025-05-16 09:19:24 +0400

    overlay effect

--- app/components/homeComponents/mainPage.jsx
+++ app/components/homeComponents/mainPage.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 import Image from "next/image"
 
 export default function mainPage() {
+    const [isClicked, setIsClicked] = useState(false)
     return (
         <>
+            {/* Overlay with transition */}
+            <div
+                className={`fixed inset-0 bg-black z-50 transition-opacity duration-300 ${isClicked ? 'opacity-50 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+                onClick={() => setIsClicked(false)}
+                style={{ transitionProperty: 'opacity' }}>
+            </div>
+            {/* Main Section */}
             <main className='flex min-h-screen items-center justify-center '>
                 <section className='flex justify-between w-[85%] max-w-[1250px]'>
                     <div className='flex flex-col justify-center gap-9'>
@@ -12,7 +21,12 @@ export default function mainPage() {
                             <span style={{color:'#6472ef'}}>AI</span> Models.
                         </h1>
                         <p className='text-gray-400 text-[18px] xl:text-3xl'>Pre-trained AI models for businesses</p>
-                        <button style={{background:'#6472ef'}} className='py-4 text-white text-sm w-1/3 hover:shadow-xl rounded-2xl xl:text-2xl'>Get Started</button>
+                        <button 
+                            style={{background:'#6472ef'}} 
+                            className='py-4 text-white text-sm w-1/3 hover:shadow-xl rounded-2xl xl:text-2xl'
+                            onClick={() => setIsClicked(true)}>
+                                Get Started
+                        </button>
                     </div>
                     <Image src='/main.png' alt='main' width={1024} height={1024} className='w-[50%] animate-float'/>
                 </section>
