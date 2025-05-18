8b5508cc47ce97091271063503ec9d7c836b3464
Dalaqa25 <g.dalaqishvili01@gmail.com>
2025-05-18 19:48:53 +0400

    model features

--- app/modelsList/[model]/page.jsx
+++ app/modelsList/[model]/page.jsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import { FiBookOpen } from 'react-icons/fi';
 
 export default async function Model({ params }) {
     return (
-        <section className='mt-17 w-[70%] max-w-[1500px] m-auto'>
+        <section className='mt-17 w-[70%] max-w-[1500px] m-auto bor'>
+            {/* header section */}
             <div className='flex'>
                 <div className="relative w-32 h-32 sm:w-70 sm:h-50">
                     <Image
@@ -21,7 +23,7 @@ export default async function Model({ params }) {
                     </div>
                 </div>
             </div>
-            <div className='flex justify-between w-[95%] m-auto'>
+            <div className='flex justify-between w-[95%] m-auto mb-10'>
                 <div className='flex w-1/2 gap-2'>
                     <p className='font-light px-3 py-3 bg-gray-100 rounded-xl '>CNN</p>
                     <p className='font-light px-3 py-3 bg-gray-100 rounded-xl '>Vision</p>
@@ -31,6 +33,31 @@ export default async function Model({ params }) {
                     <button className='text-black button bg-white shadow px-8 text-lg py-3 rounded-xl'>Download</button>
                 </div>
             </div>
+                <hr className='w-full bg-gray-100 border-0.5 border-gray-300' />
+            {/* model description */}
+            <div className='mt-10 mb-10 w-full flex flex-col gap-5'>
+                <h2 className='text-4xl font-semibold'>Model Description</h2>
+                <p className='font-light text-lg text-gray-600'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+            {/* model features */}
+                <hr className='w-full bg-gray-100 border-0.5 border-gray-300' />
+            <div className='mt-10 mb-10 w-full flex flex-col gap-5'>
+                <h2 className='text-4xl font-semibold'>Key Features</h2>
+                <div className='flex justify-between w-full'>
+                    <ul className='list-disc pl-6 flex flex-col gap-3'>
+                        <li className='font-light text-lg text-gray-600'>feature 1</li>
+                        <li className='font-light text-lg text-gray-600'>feature 1</li>
+                        <li className='font-light text-lg text-gray-600'>feature 1</li>
+                        <li className='font-light text-lg text-gray-600'>feature 1</li>
+                    </ul>
+                    <FiBookOpen className="text-9xl text-gray-700" />
+                </div>
+            </div>
+            {/* model features */}
+
         </section>
     )
 }
