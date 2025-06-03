efecb1b6695b97b1e8132ef7bd0fb5dc5f4386ee
Dalaqa25 <g.dalaqishvili01@gmail.com>
2025-06-03 10:01:25 +0400

    hidden profile

--- app/components/profilePic.jsx
+++ app/components/profilePic.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function ProfilePic() {
     return (
-        <div className="cursor-pointer bg-purple-100 w-15 h-15 mr-5 rounded-full overflow-hidden border-gray-300">
+        <div className="hidden lg:block cursor-pointer bg-purple-100 w-15 h-15 mr-5 rounded-full overflow-hidden border-gray-300">
             <Image
                 width={1024}
                 height={1024}
